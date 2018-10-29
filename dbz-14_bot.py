cffc6d1cdcc1502bb1a52123be4abf9c02f9e536
TheMazzock <44572191+TheMazzock@users.noreply.github.com>
2018-10-29 13:41:23 +0100

    Update dbz-14_bot.py

--- dbz-14_bot.py
+++ dbz-14_bot.py
@@ -1,17 +1,22 @@
-import telepot, time
+import telepot
+import time
 from telepot.loop import MessageLoop
 from pprint import pprint
+
+
 def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
     print(content_type, chat_type, chat_id)
     pprint(msg)
+    
     username = msg['from']['username']
     user_id = msg['from']['id']
     
     if content_type == 'text':
         text = msg['text']
         
-    bot.sendMessage(chat_id, text)
+    bot.sendMessage(chat_id,text)
+    
     
 TOKEN = '756616900:AAHGeFWrGvONdLWHOYCXuiS2Sb1j_XgJ9YY'
 bot = telepot.Bot(TOKEN)
